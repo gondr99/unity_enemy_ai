8cb90d2b6fac9e1bbe021376b883b33671320087
gondr99 <gondr99@gmail.com>
2025-07-27 17:59:57 +0900

    fix check arrive in astarAgent

--- Assets/01Scripts/Astar/AstarAgent.cs
+++ Assets/01Scripts/Astar/AstarAgent.cs
@@ -103,12 +103,14 @@ namespace Gondr.Astar
             Vector2 currentDirection = (destination - currentPosition).normalized;
             _beforePosition = currentPosition;
 
-            if (Vector2.Dot(beforeDirection, currentDirection) <= 0 || Vector2.Distance(destination, currentDirection) < 0.01f)
+            if (Vector2.Dot(beforeDirection, currentDirection) <= 0 || Vector2.Distance(destination, currentPosition) < 0.01f)
             {
                 _currentPathIndex++;
                 if (_currentPathIndex >= _cornerPoints.Count)
+                {
                     IsArrived = true;
-                return true;
+                    return true;
+                }
             }
 
             return false;
